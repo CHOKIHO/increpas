db596a4ee7a4c4f0e6e90f466b609608e2a14d00
USER-PC\USER <banfff@kg21.net>
2016-11-11 19:10:36 +0900

    로또번호생성기

--- Ex_1110/src/single_array/Array06.java
+++ Ex_1110/src/single_array/Array06.java
@@ -10,27 +10,41 @@ public class Array06 {
 		// 1~45사이의 난수를 발생시켜 로또번호 생성기 만들기 (중복없음)
 		int[] lotto = new int[6];
 
-		for (int i = 0; i < lotto.length; i++) {
+		//for (int i = 0; i < lotto.length;) {
+		
+		outer : for (int i = 0; i < lotto.length;) {
+			//lotto[i] = new Random().nextInt(45) + 1;
 
-			lotto[i] = new Random().nextInt(45) + 1;
-
-			for (int j = 0; j < i; j++) {
+			
+/*			for (int j = 0; j < i; j++) {
 				if (lotto[i] == lotto[j]) {
 					// System.out.print("duplicate("+ lotto[i] + ") ");
 					i--;
 					break;
 				}
 			}
+*/			
+			lotto[i] = new Random().nextInt(6) + 1;
+			
+			for (int j=0;j<i;j++) {
+				if (lotto[i] == lotto[j]) {
+					continue outer;  //outer의 증감식으로 이동
+					//break;
+				}
+			}
+			
+			System.out.printf("%02d ", lotto[i]);
+			i++;
 		}
 
-		for (int i : lotto) {
-			System.out.print(i + " ");
-		}
-		
-		System.out.println();
-		
-		Arrays.sort(lotto);
-		System.out.println(Arrays.toString(lotto));
+//		for (int i : lotto) {
+//			System.out.print(i + " ");
+//		}
+//		
+//		System.out.println();
+//		
+//		Arrays.sort(lotto);
+//		System.out.println(Arrays.toString(lotto));
 	}
 
 }
