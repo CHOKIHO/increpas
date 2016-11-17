d6dadcfbdfc385e9b8682011f4e6ae50029aaf0f
USER-PC\USER <banfff@kg21.net>
2016-11-17 17:56:35 +0900

    제네릭 - 메서드편

---
+++ Ex_1117/src/generic02/GenMain.java
@@ -0,0 +1,30 @@
+package generic02;
+
+public class GenMain {
+
+	public static void main(String[] args) {
+
+		Integer[] iArr = { 1, 2, 3, 4, 5 };
+		Double[] dArr = { 1.1, 2.2, 3.3, 4.4, 5.5 };
+		Character[] cArr = { 'A', 'B', 'C', 'D', 'E' };
+		
+/*		Gen<Integer> gen1 = new Gen<Integer>();
+		gen1.printArr(iArr);
+		
+		//실수출력
+		Gen<Double> gen2 = new Gen<Double>();
+		gen2.printArr(dArr);
+		
+		//문자출력
+		Gen<Character> gen3 = new Gen<Character>();
+		gen3.printArr(cArr);
+*/		
+
+		Gen gen = new Gen();
+		gen.printArr(iArr);
+		gen.printArr(dArr);
+		gen.printArr(cArr);
+		
+	}
+
+}
